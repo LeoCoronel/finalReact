8b48d12f53cbd7bc3bca45198aa5fe41ababc571
Leo Coronel <leomcoronel99@gmail.com>
2023-05-05 17:46:34 -0300

    loader

--- src/Components/GalleryShop/GalleryShop.jsx
+++ src/Components/GalleryShop/GalleryShop.jsx
@@ -1,41 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import ShoeCard from '../ShoeCard/ShoeCard';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchShoes } from '../../redux/slices/shoesSlice';
-import { addToCart } from '../../redux/slices/cartSlice';
+import React, { useEffect, useState } from "react";
+import ShoeCard from "../ShoeCard/ShoeCard";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchShoes } from "../../redux/slices/shoesSlice";
+import { addToCart } from "../../redux/slices/cartSlice";
 
 const GalleryShop = () => {
-    const dispatch = useDispatch();
-    const shoes = localStorage.getItem('shoes') ? JSON.parse(localStorage.getItem('shoes')) : useSelector((state) => state.shoes.data);    
+  const dispatch = useDispatch();
+  const shoes = localStorage.getItem("shoes")
+    ? JSON.parse(localStorage.getItem("shoes"))
+    : useSelector((state) => state.shoes.data);
 
-    const onAdd = (product) => {
-        dispatch(addToCart(product));
+  const onAdd = (product) => {
+    dispatch(addToCart(product));
+  };
+
+  useEffect(() => {
+    console.log("SHOES DAAT", shoes);
+    if (!shoes) {
+      console.log("HAY QUE FETCHEAR");
+      dispatch(fetchShoes());
+    } else {
+      console.log("NO SE FETCHEA");
     }
+    localStorage.setItem("shoes", JSON.stringify(shoes));
+  }, []);
 
-    useEffect(() => {
-        console.log("SHOES DAAT", shoes)
-        if(!shoes) {
-            console.log("HAY QUE FETCHEAR")
-            dispatch(fetchShoes());
-        } else {
-            console.log("NO SE FETCHEA")
-        }
-        localStorage.setItem('shoes', JSON.stringify(shoes));
-    }, [])
+  return (
+    <div className="shop__shoes">
+      {shoes ? (
+        shoes?.map((shoe) => {
+          return <ShoeCard key={shoe.id} shoe={shoe} onAdd={onAdd} />;
+        })
+      ) : (
+        <p>buscando</p>
+      )}
+    </div>
+  );
+};
 
-    return (
-        <div className='shop__shoes'>
-            {shoes && shoes?.map((shoe) => {
-                return (    
-                    <ShoeCard 
-                        key={shoe.id}
-                        shoe={shoe}
-                        onAdd={onAdd}
-                    />
-                )
-            })}
-        </div>
-    )
-}
-
-export default GalleryShop
+export default GalleryShop;
